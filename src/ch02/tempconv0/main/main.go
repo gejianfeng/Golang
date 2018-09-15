28d9df42675205962b9348b905f8c011bf223194
unknown <you@example.com>
2018-09-16 00:11:03 +0800

    + regroup source folder struct + add ch02/tempconv0 + add ch02/tempconv1

---
+++ src/ch02/tempconv0/main/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"ch02/tempconv0/tempconv"
+)
+
+func main() {
+	fmt.Printf("%g\n", tempconv.BoilingC - tempconv.FreezingC)
+	boilingF := tempconv.CToF(tempconv.BoilingC)
+	fmt.Printf("%g\n", boilingF - tempconv.CToF(tempconv.FreezingC))
+	//fmt.Printf("%g\n", boilingF - tempconv.FreezingC)
+
+	var c tempconv.Celsius
+	var f tempconv.Fahrenheit
+
+	fmt.Println(c == 0)
+	fmt.Println(f >= 0)
+	//fmt.Println(c == f)
+	fmt.Println(c == tempconv.Celsius(f))
+
+	var v = tempconv.FToC(212.0)
+	//fmt.Println(v.String())
+	fmt.Printf("%v\n", v)
+	fmt.Printf("%s\n", v)
+	//fmt.Printf(v)
+	fmt.Printf("%g\n", v)
+	fmt.Println(float64(v))
+}
