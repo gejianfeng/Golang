bdbcba58450497931d1a25a4a355247305f7c021
unknown <you@example.com>
2018-09-18 00:02:58 +0800

    + add ch01/fetch + add ch01/ex1.7 + add ch01/ex1.8 + add ch01/ex1.9

---
+++ src/ch01/ex1.9/ex1.9.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"net/http"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(resp.Status)
+	}
+}
